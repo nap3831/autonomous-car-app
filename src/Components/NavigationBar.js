f5a550a3e44b72e185755e6343603495b70ab70c
nap3831 <nap3831@gmail.com>
2019-11-17 15:10:11 +0700

    create navbar and edit styles

--- src/Components/NavigationBar.js
+++ src/Components/NavigationBar.js
@@ -1,64 +1,51 @@
 import React, { Component } from 'react';
-import { Collapse, Navbar, NavbarToggler, Nav, NavItem } from 'reactstrap';
+import { Navbar, Nav, NavItem } from 'reactstrap';
 import { NavLink } from 'react-router-dom';
 class NavigationBar extends Component {
 
     constructor(props) {
         super(props);
         this.state = {
-            isOpen: false,
-            scrolling: false,
-            isHover: false,
-            search: false,
-            isClick: false,
         }
-        this.closeNav = this.closeNav.bind(this);
-        this.toggle = this.toggle.bind(this);
-    }
-    handleClickCart(){
-        this.setState({
-            isClick: !this.state.isClick
-        })
-    }
-    toggle() {
-        this.setState({
-            isOpen: !this.state.isOpen
-        })
-    }
-    closeNav() {
-        this.setState({
-            isOpen: false,
-        })
     }
     render() {
-     
+
         return (
             <React.Fragment>
-                <div className="container">
-                    <Navbar className="text-center  alpha  fixed-top"  light expand="lg">
-                        {/* <NavLink className="navbar-brand" to="/">
-                            <img src={mainLogo} className="logo" alt="logo" />
-                        </NavLink> */}
-                        <NavbarToggler onClick={this.toggle} />
-                        <Collapse isOpen={this.state.isOpen} navbar>
-                            <div className="alignAutoRight">
-                                <Nav navbar>
-                                    <NavItem className="navbarLink">
-                                        <NavLink to="/Home" onClick={this.closeNav}>หน้าหลัก</NavLink>
-                                    </NavItem>
-                                    <NavItem className="navbarLink">
-                                        <NavLink to="/Product" onClick={this.closeNav}>สินค้าทั้งหมด</NavLink>
-                                    </NavItem>
-                                    <NavItem className="navbarLink">
-                                        <NavLink to="/News" onClick={this.closeNav}>สินค้าใหม่</NavLink>
-                                    </NavItem>
-                                    <NavItem className="navbarLink">
-                                        <NavLink to="/Promotion" onClick={this.closeNav}>สินค้าโปรโมชั่น</NavLink>
-                                    </NavItem>
-                                </Nav>
-                            </div>
-                        </Collapse>
-                    </Navbar>
+                <div className="container-fluid fixed-bottom alpha">
+                    <div className="row text-center">
+                        <div className="col-3">
+                            <NavItem className="navbarLink">
+                                <img src="https://image.flaticon.com/icons/svg/2242/2242111.svg" width="20px" alt="logo" />
+                                <br/>
+                                <NavLink to="/Home" onClick={this.closeNav}>หน้าหลัก</NavLink>
+                            </NavItem>
+
+                        </div>
+                        <div className="col-3">
+                            <NavItem className="navbarLink">
+                                <img src="https://image.flaticon.com/icons/svg/2242/2242111.svg" width="20px" alt="logo" />
+                                <br/>
+                                <NavLink to="/Product" onClick={this.closeNav}>สินค้าทั้งหมด</NavLink>
+                            </NavItem>
+
+                        </div>
+                        <div className="col-3">
+                            <NavItem className="navbarLink">
+                                <img src="https://image.flaticon.com/icons/svg/2242/2242111.svg" width="20px" alt="logo" />
+                                <br/>
+                                <NavLink to="/News" onClick={this.closeNav}>สินค้าใหม่</NavLink>
+                            </NavItem>
+
+                        </div>
+                        <div className="col-3">
+                            <NavItem className="navbarLink">
+                                <img src="https://image.flaticon.com/icons/svg/2242/2242111.svg" width="20px" alt="logo" />
+                                <br/>
+                                <NavLink to="/Promotion" onClick={this.closeNav}>สินค้าโปรโมชั่น</NavLink>
+                            </NavItem>
+                        </div>
+                    </div>
                 </div>
             </React.Fragment>
         );
